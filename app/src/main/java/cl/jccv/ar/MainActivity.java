47ccbb7d83c1a9f0fecfaaa9d1bd0418dd42eed7
Sebastian Velasquez <sebastian.v.a@riseup.net>
2020-02-19 21:11:14 +0100

    modelo 3d con menos brillo

--- app/src/main/java/cl/jccv/ar/MainActivity.java
+++ app/src/main/java/cl/jccv/ar/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         arFragment.setOnTapArPlaneListener(((hitResult, plane, motionEvent) -> {
             Anchor anchor = hitResult.createAnchor();
             ModelRenderable.builder()
-                    .setSource(this, Uri.parse("matapaColor.sfb"))
+                    .setSource(this, Uri.parse("matapaColor2.sfb"))
                     .build()
                     .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable))
                     .exceptionally(throwable -> {
